9a3643a959b6f8ae0611b765a728af56f88a4946
kana sasazawa <kana.yellow7@gmail.com>
2025-03-30 12:44:22 +0900

    演習１、２

---
+++ src/iphone/co/jp/SmartPhone.java
@@ -0,0 +1,17 @@
+package iphone.co.jp;
+
+public class SmartPhone extends Mp3Player {
+	//演習２
+	public void call() {
+		System.out.println("電話");
+	}
+	public void mail() {
+		System.out.println("メール");
+	}
+	public void photo() {
+		System.out.println("写真");
+	}
+	public void internet() {
+		System.out.println("インターネット");
+	}
+}
