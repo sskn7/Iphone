0dd479ea864b01163ede6d513d382af7baa871b6
kana sasazawa <kana.yellow7@gmail.com>
2025-03-30 12:51:36 +0900

    演習４

--- src/iphone/co/jp/SmartPhone.java
+++ src/iphone/co/jp/SmartPhone.java
@@ -14,4 +14,18 @@ public class SmartPhone extends Mp3Player {
 	public void internet() {
 		System.out.println("インターネット");
 	}
+	
+	//演習４　オーバーライド(上書き)
+	public void play() {
+		System.out.println("再生(ハイレゾ機能)");
+	}
+	public void stop() {
+		System.out.println("停止(ハイレゾ機能)");
+	}
+	public void next() {
+		System.out.println("次へ(ハイレゾ機能)");
+	}
+	public void back() {
+		System.out.println("戻る(ハイレゾ機能)");
+	}
 }
